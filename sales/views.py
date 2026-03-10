a1697e17aa08f0a38ab57fa19ab266c411fbb30e
aglaxman <laxmannaagoudar8@gmail.com>
2026-03-10 17:32:16 +0530

    creating UI , and refining the BL

--- sales/views.py
+++ sales/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def dashboard(req):
+    return render(req,'dashboard.html')
+
+def product_list(req):
+    return render(req,'products/product_list.html')
--- vikmo/settings.py
+++ vikmo/settings.py
@@ -38,7 +38,16 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "sales",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -55,7 +64,7 @@ ROOT_URLCONF = "vikmo.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
